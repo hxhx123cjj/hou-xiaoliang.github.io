968a888d2c2ceb67cf83c2667f746579518731a8
hou-xiaoliang <858145049@qq.com>
2019-12-19 14:57:57 +0800

    Test Commit

--- public/js/prism.js
+++ public/js/prism.js
@@ -54,8 +54,8 @@ function loadStyle(url) {
         .then(function () {
           loadStyle('//cdn.jsdelivr.net/npm/prismjs/plugins/toolbar/prism-toolbar.min.css');
           Prism.plugins.toolbar.registerButton('select-code', function (env) {
-            var button = document.createElement('button');
-            button.innerHTML = 'select this copy' + env.language;
+            var button = document.createElement('Copy to Button');
+            button.innerHTML = 'select this ' + env.language;
             button.addEventListener('click', function () {
               // Source: http://stackoverflow.com/a/11128179/2757940
               if (document.body.createTextRange) { // ms
